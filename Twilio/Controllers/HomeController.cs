f77f4209a697b04584df55868afa0c4037784595
Ravi Pathuri <Ravi.Pathauri@outlook.com>
2014-01-22 20:59:47 +0530

    adding TwiML changes

--- Twilio/Controllers/HomeController.cs
+++ Twilio/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace Twilio.Controllers
 {
@@ -30,12 +31,29 @@ namespace Twilio.Controllers
 
         public ActionResult Incoming()
         {
+            var doc = new XDocument();
 
-            return new ContentResult
-            {
-                Content = "",
-                ContentType = "text/xml"
-            };
+            var response = new XElement("Response");
+            var gather = new XElement("Gathter",
+                    new XAttribute("action", Url.Action("Input")),
+                    new XElement("Say", "Thank you for calling. Please enter your policy number followed by pound key.")
+                );
+            response.Add(gather);
+            doc.Add(response);
+
+            return TwiML(doc);
+        }
+
+        public ActionResult Input()
+        {
+            var doc = new XDocument();
+
+            var response = new XElement("Response");
+            var gather = new XElement("Say", "Thank you for your support.");
+            response.Add(gather);
+            doc.Add(response);
+
+            return TwiML(doc);
         }
     }
 }
