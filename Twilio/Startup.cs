fc5b97bd40f614876b312e1bb1a21e565759763f
Ravi Pathuri <Ravi.Pathauri@outlook.com>
2014-01-22 20:31:07 +0530

    Adding files

---
+++ Twilio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Twilio.Startup))]
+namespace Twilio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
